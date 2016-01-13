d5f088abf28f850e6adf2e6e386575d76a6369bc
Jai Jaswani <jaswanij@vmware.com>
2016-01-12 17:56:22 -0800

    Leetcode: Delete node in a linked list
    Problem:
    https://leetcode.com/problems/delete-node-in-a-linked-list/
    
    Solution:
    https://leetcode.com/submissions/detail/41047712/

---
+++ Leetcode/src/main/java/leetcode/easy/DeleteNodeInLinkedList.java
@@ -0,0 +1,27 @@
+package leetcode.easy;
+
+/**
+ * Created by jai around 9/28/15.
+ * Write a function to delete a node (except the tail) in a singly linked list, given only access to that node.
+ * Supposed the linked list is 1 -> 2 -> 3 -> 4 and you are given the third node with value 3, the linked list
+ * should become 1 -> 2 -> 4 after calling your function.
+ */
+public class DeleteNodeInLinkedList {
+    public class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(int x) {
+            val = x;
+        }
+    }
+
+    public void deleteNode(ListNode node) {
+        if (node.next == null) {
+            return;
+        }
+        node.val = node.next.val;
+        node.next = node.next.next;
+
+    }
+}
