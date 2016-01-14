f77e6a0f9e59b2e36c3a64958fcc6ee8e05cad21
Jai Jaswani <jaswanij@vmware.com>
2016-01-13 17:20:10 -0800

    Leetcode: Merge Sorted Array
    Problem:
    https://leetcode.com/problems/merge-sorted-array/
    
    Solution:
    https://leetcode.com/submissions/detail/41627158/

---
+++ Leetcode/src/main/java/leetcode/easy/MergeSortedArray.java
@@ -0,0 +1,26 @@
+package leetcode.easy;
+
+/**
+ * Created by jai around 9/28/15.
+ * Given two sorted integer arrays nums1 and nums2, merge nums2 into nums1 as one sorted array.
+ *
+ * Note:
+ * You may assume that nums1 has enough space (size that is greater or equal to m + n) to hold additional elements from
+ * nums2. The number of elements initialized in nums1 and nums2 are m and n respectively.
+ */
+public class MergeSortedArray {
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+        int i = m + n - 1;
+        m--; n--;
+        while(m >= 0 && n >= 0) {
+            if(nums1[m] > nums2[n]) {
+                nums1[i--] = nums1[m--];
+            } else {
+                nums1[i--] = nums2[n--];
+            }
+        }
+        while(n >= 0) {
+            nums1[i--] = nums2[n--];
+        }
+    }
+}
